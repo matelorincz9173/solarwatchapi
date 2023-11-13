f2920af8f26fee69d5d148875f26787d51644791
matelorincz9173 <lorincz.mate.to@gmail.com>
2023-11-13 12:19:43 +0100

    add starter code

---
+++ SolarwatchAPI/SolarwatchAPI/Service/CoordinatesToSolar/CoordinateToSolarConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using SolarwatchAPI.Model;
+
+namespace SolarwatchAPI.Service.CoordinatesToSolar;
+
+public class CoordinateToSolarConverter
+{
+    private HttpClient client = new();
+    
+    public async Task<string> ConvertCoordinatesToSolar(Coordinate coordinate, DateTime date, string sunsetOrSunrise)
+    {
+
+        try
+        {
+            string url = $"https://api.sunrise-sunset.org/json?lat={coordinate.Lat}&lng={coordinate.Lon}&date={date}";
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                using (JsonDocument doc = JsonDocument.Parse(content))
+                {
+                    var root = doc.RootElement;
+                    return root.GetProperty("results").GetProperty(sunsetOrSunrise).GetString();
+                }
+            }
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        return "{}";
+    }
+}
